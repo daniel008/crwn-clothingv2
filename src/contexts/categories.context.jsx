a044f4aa4588de106cc320c312155168009b4b9f
DanielMa <mwldaniel_008@126.com>
2023-01-26 14:27:36 +0800

    create category-preview component && nested routes in shop component

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,26 +1,26 @@
 import { createContext, useState, useEffect } from 'react'
 import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils'
 
-export const ProductsContext = createContext({
-  products: [],
-  setProducts: () => {},
+export const CategoriesContext = createContext({
+  categoriesMap: {},
 })
 
-export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([])
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({})
 
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments()
       console.log(categoryMap)
+      setCategoriesMap(categoryMap)
     }
     getCategoriesMap()
   }, [])
 
-  const value = { products, setProducts }
+  const value = { categoriesMap }
   return (
-    <ProductsContext.Provider value={value}>
+    <CategoriesContext.Provider value={value}>
       {children}
-    </ProductsContext.Provider>
+    </CategoriesContext.Provider>
   )
 }
